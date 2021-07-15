29ab7235ddfa46e95ec05fabf6de6ae746e0633d
Robson Gomes <nosbor84@gmail.com>
2021-07-15 19:03:54 -0300

    soluçao final do desafio

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,21 @@
-import {
+import React, {
   useEffect,
   useRef,
   useState,
   useCallback,
-} from 'react';
+  InputHTMLAttributes,
+} from "react";
 
-import { useField } from '@unform/core';
+import { useField } from "@unform/core";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+}
+
+const Input = ({ name, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -31,13 +36,13 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
+      path: "value",
     });
   }, [fieldName, registerField]);
 
   return (
     <Container isFilled={isFilled} isFocused={isFocused}>
-      {Icon && <Icon size={20} />}
+      {/* {Icon && <Icon size={20} />} */}
 
       <input
         onFocus={handleInputFocus}
---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export interface Food {
+  id: number;
+  name: string;
+  description: string;
+  available: boolean;
+  price: number;
+  image: string;
+}
+
+export interface Zaa {
+  id: number;
+}
